72b6e2a1538988781f8845bd57fc29fb6a10d659
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-11-14 08:16:50 -0300

    chore: estrutura inicial

---
+++ src/main/kotlin/br/com/arquitetura/hexagonal/ArquiteturaHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package br.com.arquitetura.hexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArquiteturaHexagonalApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArquiteturaHexagonalApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/arquitetura/hexagonal/ArquiteturaHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.arquitetura.hexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArquiteturaHexagonalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
